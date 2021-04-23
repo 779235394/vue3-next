1868c477fcbf482243abee4789d541e83b7c14f7
junlan.he <779235394@qq.com>
2021-04-23 15:37:20 +0800

    配置修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import {resolve} from 'path';
 
+/**
+ * @param {dir} dir is File path.
+ * @return {dir} dir is File path.
+ */
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir);
+}
 export default defineConfig({
+  resolve: {
+    alias: [
+      // /@/xxxx => src/xxxx
+      {
+        find: /\/@\//,
+        replacement: pathResolve('src') + '/'
+      }
+    ]
+  },
   plugins: [vue()]
-})
+});

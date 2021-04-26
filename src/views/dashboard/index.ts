ba8cb36012b3aec1985237525d706725cda448be
wo732511064 <732511064@qq.com>
2021-04-26 12:00:39 +0800

    测试

--- src/views/dashboard/index.ts
+++ src/views/dashboard/index.ts
@@ -3,13 +3,13 @@
  * @Author: huangzihong
  * @Date: 2021-04-26 10:45:24
  * @LastEditors: huangzihong
- * @LastEditTime: 2021-04-26 11:57:55
+ * @LastEditTime: 2021-04-26 12:00:25
  */
-import { defineComponent, computed } from 'vue'
+import {defineComponent, computed} from 'vue';
 
 export default defineComponent({
   name: 'Home',
   setup: () => {
-    return {}
+    return {};
   }
-})
+});

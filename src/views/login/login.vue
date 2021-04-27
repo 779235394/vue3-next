2a7b9643926d2999ccee953fb7ba3a852b4f0487
年少轻狂 <39368765+779235394@users.noreply.github.com>
2021-04-27 14:56:42 +0800

    登录跳转

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -6,9 +6,9 @@
 <template>
   <Particles id='tsparticles' :options='ParticlesOptions' class='particles' />
   <div class='login-container'>
-    <el-form :model='ruleFormModel' label-width='left'  status-icon ref='ruleForm' class='login-form'>
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
+    <el-form :model='ruleFormModel' label-width='left' status-icon ref='ruleForm' class='login-form'>
+      <div class='title-container'>
+        <h3 class='title'>Login Form</h3>
       </div>
       <el-form-item label='账号' prop='pass'>
         <el-input type='text' prefix-icon='el-icon-user' v-model='ruleFormModel.account' autocomplete='off'></el-input>
@@ -20,7 +20,7 @@
           v-model='ruleFormModel.password'
           autocomplete='off'></el-input>
       </el-form-item>
-      <el-button  type="primary" style="width:100%;margin-bottom:30px;">Login</el-button>
+      <el-button type='primary' style='width:100%;margin-bottom:30px;' @click='onsubmit'>Login</el-button>
     </el-form>
   </div>
 </template>
@@ -28,14 +28,23 @@
 <script>
 import { defineComponent } from 'vue'
 import { ruleForm, ParticlesOptions } from './login'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
   name: 'Login',
 
   setup: () => {
+    const router = useRouter()
+    const onsubmit = () => {
+      if (ruleForm.account === 'admin' && ruleForm.password === '123456') {
+        console.log(router)
+        router.push({ path: 'dashboard' })
+      }
+    }
     return {
       ruleFormModel: ruleForm,
       ParticlesOptions,
+      onsubmit,
     }
   },
 })
